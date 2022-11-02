fff180181a01e6ee36cfbe7bc80fc382b4740205
Castersen <collegejcastersen@gmail.com>
2022-11-01 19:02:41 -0700

    Close naive method

--- p1/algorithms.cpp
+++ p1/algorithms.cpp
@@ -2,21 +2,45 @@
 
 // Standard Matrix Multiply O(n^3)
 matrix_2d classic_matrix_multiply(matrix_2d m1, matrix_2d m2) {
-	matrix_2d result;
-	std::vector<int> temp;
-	int acc = 0, N = m1.size();
+	matrix_2d result = fill_matrix_with_zeroes(m1.size());
+	int N = m1.size();
 
-	for(int i = 0; i < N; ++i) {
-		for(int j = 0; j < N; ++j) {
-			for(int k = 0; k < N; ++k) {
-				acc += m1[i][k] * m2[k][j];
-			}
-			temp.push_back(acc);
-			acc = 0;
-		}
-		result.push_back(temp);
-		temp.clear();
-	}
+	for(int i = 0; i < N; ++i)
+		for(int j = 0; j < N; ++j)
+			for(int k = 0; k < N; ++k)
+				result[i][j] += m1[i][k] * m2[k][j];
 
 	return result;
+}
+
+matrix_2d naive_divide_and_conquer(matrix_2d m1, matrix_2d m2) {
+	matrix_2d C = fill_matrix_with_zeroes(m1.size());
+	int N = m1.size();
+
+	if(N == 1) {
+		C[0][0] = m1[0][0] * m2[0][0];
+		return C;
+	} else {
+		matrix_2d A11,A12,A21,A22,
+				  B11,B12,B21,B22;
+
+		std::tie(A11,A12,A21,A22) = split_matrix(m1);
+		std::tie(B11,B12,B21,B22) = split_matrix(m2);
+
+		matrix_2d C11 = add_2d_matrix(naive_divide_and_conquer(A11,B11),
+									  naive_divide_and_conquer(A12,B21));
+
+		matrix_2d C12 = add_2d_matrix(naive_divide_and_conquer(A11,B12),
+									  naive_divide_and_conquer(A12,B22));
+
+		matrix_2d C21 = add_2d_matrix(naive_divide_and_conquer(A21,B11),
+									  naive_divide_and_conquer(A22,B21));
+
+		matrix_2d C22 = add_2d_matrix(naive_divide_and_conquer(A21,B12),
+									  naive_divide_and_conquer(A22,B22));
+
+		C = combine_matrix(C11, C12, C21, C22);
+	}
+
+	return C;
 }
--- p1/main.cpp
+++ p1/main.cpp
@@ -1,16 +1,18 @@
 #include "algorithms.h"
 #include "matrix_operations.h"
+#include <iostream>
 
 int main() {
 	matrix_2d t1 = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
 	matrix_2d t2 = {{2,2,2,2}, {4,4,3,3}, {6,1,2,4}, {9,8,1,2}};
 
 	print_2d_matrix(classic_matrix_multiply(t1, t2));
-    matrix_2d a,b,c,d;
-	std::tie(a,b,c,d) = split_matrix(t1);
+	std::cout << std::endl;
 
-	print_2d_matrix(a);
-	print_2d_matrix(b);
-	print_2d_matrix(c);
-	print_2d_matrix(d);
+	matrix_2d a,b,c,d;
+	std::tie(a,b,c,d) = split_matrix(t1);
+	print_2d_matrix(combine_matrix(a,b,c,d));
+
+	std::cout << std::endl;
+	print_2d_matrix(naive_divide_and_conquer(t1, t2));
 }
