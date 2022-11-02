ec8fbc326e9437b075fa456ef8d2ebc41de7a840
Castersen <collegejcastersen@gmail.com>
2022-11-02 13:33:01 -0700

    matrix power constant

--- p1/main.cpp
+++ p1/main.cpp
@@ -11,28 +11,29 @@ int main() {
 	// matrix_2d t2 = {{0,1,6,3}, {-2,8,7,1}, {2,0,-1,0}, {9,1,6,-2}};
 
 	matrix_2d t1, t2;
+	int matrix_power = 2;
 
-	for(int i = 1; i <= 15; ++i) {
-		std::tie(t1, t2) = generate_random_matrices(pow(3, i));
-		int number = pow(2, std::ceil(std::log(pow(3,i))/std::log(2)));
+	for(int i = 1; i <= 1; ++i) {
+		std::tie(t1, t2) = generate_random_matrices(pow(matrix_power, i));
+		int number = pow(2, std::ceil(std::log(pow(matrix_power,i))/std::log(2)));
 		pad_matrices_till_square(t1, t2);
 
 	    auto start = std::chrono::high_resolution_clock::now();
-		classic_matrix_multiply(t1, t2);
+		print_2d_matrix(classic_matrix_multiply(t1, t2));
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		std::cout << "Algo: Classic Matrix Multiply " << "Number: " << number
 					 <<  " Time: " << duration.count() << " microseconds" << std::endl;
 
 	    start = std::chrono::high_resolution_clock::now();
-		naive_divide_and_conquer(t1, t2);
+		print_2d_matrix(naive_divide_and_conquer(t1, t2));
 		stop = std::chrono::high_resolution_clock::now();
 		duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		std::cout << "Algo: Naive Divide and Conquer Matrix Multiply " << "Number: " << number
 					 <<  " Time: " << duration.count() << " microseconds" << std::endl;
 
 	    start = std::chrono::high_resolution_clock::now();
-		strassen_multiply(t1.size(), t1, t2);
+		print_2d_matrix(strassen_multiply(t1.size(), t1, t2));
 		stop = std::chrono::high_resolution_clock::now();
 		duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 		std::cout << "Algo: Strassen Matrix Multiply " << "Number: " << number
