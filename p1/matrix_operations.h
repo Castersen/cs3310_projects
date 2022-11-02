54525ad5cc2e1b5996003f049ce5d9409bab32af
OrbinWaffle <joshlaiguy@gmail.com>
2022-11-01 20:55:34 -0700

    updating matrix_operations.h

--- p1/matrix_operations.h
+++ p1/matrix_operations.h
@@ -8,9 +8,11 @@
 typedef std::vector<std::vector<int>> matrix_2d;
 
 void print_2d_matrix(matrix_2d m);
+void pad_2d_matrix(matrix_2d& m);
 matrix_2d slice_2d_matrix(matrix_2d m, int x0, int x1, int y0, int y1);
 matrix_2d add_2d_matrix(matrix_2d m1, matrix_2d m2);
 matrix_2d subtract_2d_matrix(matrix_2d m1, matrix_2d m2);
+std::vector<int> fill_array_with_zeroes(int size);
 matrix_2d fill_matrix_with_zeroes(int size);
 std::tuple<matrix_2d, matrix_2d, matrix_2d, matrix_2d> split_matrix(matrix_2d m);
 std::vector<int> add_1d_array(std::vector<int> a1, std::vector<int> a2);
